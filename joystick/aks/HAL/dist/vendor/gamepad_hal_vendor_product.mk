67705ded30bb552953a6d9953da60a2d1c4099cf
junj <quic_junj@quicinc.com>
2023-05-24 10:18:52 +0530

    gamepad_hal_vendor_product makefile only for target kalama
    Change-Id: I0cada6138c3888e3c20343be5987e76c1fc32e41

--- joystick/aks/HAL/dist/vendor/gamepad_hal_vendor_product.mk
+++ joystick/aks/HAL/dist/vendor/gamepad_hal_vendor_product.mk
@@ -1,6 +1,8 @@
+ifeq ($(call is-board-platform-in-list, kalama), true)
 PRODUCT_PACKAGES += vendor.aks.gamepad@1.0 \
      vendor.aks.gamepad@1.0-impl \
      vendor.aks.gamepad@1.0-service \
 
 PRODUCT_PACKAGES += vendor.aks.gamepad@1.0-service.rc
 PRODUCT_PACKAGES += init.vendor.aks.rc
+endif
