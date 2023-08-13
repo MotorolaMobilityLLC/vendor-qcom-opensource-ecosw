0c9c144a3a6a2e0023dd2010aea821bd5966c70d
Mohit Sharma <quic_smohit@quicinc.com>
2023-08-13 10:21:53 +0530

    Disable AKS for Kalama.
    Change-Id: Ie0c300e5004e1358779a5d5599c6be68875232f9

--- joystick/aks/HAL/dist/vendor/gamepad_hal_vendor_product.mk
+++ joystick/aks/HAL/dist/vendor/gamepad_hal_vendor_product.mk
@@ -1,8 +1,8 @@
 ifeq ($(call is-board-platform-in-list, kalama), true)
-PRODUCT_PACKAGES += vendor.aks.gamepad@1.0 \
+#PRODUCT_PACKAGES += vendor.aks.gamepad@1.0 \
      vendor.aks.gamepad@1.0-impl \
      vendor.aks.gamepad@1.0-service \
 
-PRODUCT_PACKAGES += vendor.aks.gamepad@1.0-service.rc
-PRODUCT_PACKAGES += init.vendor.aks.rc
+#PRODUCT_PACKAGES += vendor.aks.gamepad@1.0-service.rc
+#PRODUCT_PACKAGES += init.vendor.aks.rc
 endif
